f7e960f6cbce7598b8abd001295a8be4f63f6aa3
MiguelAngelCordoba <miguelcordoba.ds@gmail.com>
2026-09-02 13:10:02 -0500

    chore: limpiar next/ y excluir DBTOOLS$MCP_LOG antes de consolidar la release

--- db/dist/releases/next/changes/fix/6/veranolink/indexes/idx_tbl_activity_activity.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
--- changeset VERANOLINK:1788300191272 stripComments:false  logicalFilePath:fix\6\veranolink\indexes\idx_tbl_activity_activity.sql
--- sqlcl_snapshot db/src/database/veranolink/indexes/idx_tbl_activity_activity.sql:null:b6d73bb11388251e55e3e523e1faca29d5d614a1:create
-
-create index veranolink.idx_tbl_activity_activity on
-    veranolink.tbl_activity (
-        activity_id
-    );
-
